bd9d3560ec11c0ed49f3b5519364425393719c3b
FarisJR <farisjramadhan@gmail.com>
2021-08-03 07:09:14 +0700

    Fix payment method models

--- models/paymentMethods.go
+++ models/paymentMethods.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type PaymentMethods struct {
 	gorm.Model
-	ID     int    `json:"id" form:"id"`
+	ID     int    `gorm:"primaryKey"`
 	Name   string `json:"name" form:"name"`
 	Status string `json:"status" form:"status"`
 
